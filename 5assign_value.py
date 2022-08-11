69f4373ceaac70af3dde29772204eaa85a5386be
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-08-11 23:01:24 +0530

    Add files via upload

---
+++ 5assign_value.py
@@ -0,0 +1,26 @@
+from re import L
+
+
+a = "63"
+
+print(float(a))
+
+
+a = 1 if 20 > 10 else 0 #in c we write int a = 20 > 10 ? 1 : 0;(conditional operater)
+
+# Printing value of a
+print("The value of a is: " , str(a))
+
+ 
+print("geeks", end =" ")
+print("geeksforgeeks")
+ 
+# array
+l = [1, 2, 3, 4]
+ 
+# printing a element in same
+# line
+# for i in range(4):
+#     print(a[i], end =" ")
+
+print(*l)
