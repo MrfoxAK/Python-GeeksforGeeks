bac2ef81ad4c0f8ab95fadee2c1da3e5f2b11033
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-08-11 23:00:54 +0530

    Add files via upload

---
+++ 3structuring.py
@@ -0,0 +1,91 @@
+x=[1,2,3,4,5]
+
+
+print(x[1:4])
+
+
+
+a = 10; b = 20; c = b + a
+  
+print(a,end=" "); print(b); print(c)
+
+l = [
+     [1,2,3],
+     [3,4,[4,6],5],
+     [5,6,7]
+     ]
+
+print(l)
+
+
+person_1 = 127
+person_2 = 147
+person_3 = 7
+
+if (
+   person_1 >= 18 and
+   person_2 >= 18 and
+   person_3 < 18
+   ):
+    print('2 Persons should have ID Cards')
+
+x = \
+    1 + 2 \
+    + 5 + 6 \
+    + 10 \
+     + 34
+
+print(x)
+
+a = 'This is # not a comment #'
+print(a) 
+
+
+a = 1-2  # Better way is a = 1 - 2
+  
+print(a)
+
+
+  
+x = [1, 2, 3]
+y = 2
+  
+""" Following is incorrect, and will generate syntax error
+a = yin x
+"""
+  
+# Corrected version is written as
+a = y in x
+print(a)
+
+
+
+print('foo') # Correct
+  
+#    print('foo')
+
+
+
+  
+x = 10
+  
+while(x != 0):  
+ if(x > 5):   # Line 1
+  print('x > 5')  # Line 2
+ else:        # Line 3
+  print('x < 5') # Line 4
+ x -= 2       # Line 5
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
