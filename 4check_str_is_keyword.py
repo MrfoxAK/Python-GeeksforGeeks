fe2b7938195a97f12c1a274a83a598835574d8d5
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-08-11 23:01:11 +0530

    Add files via upload

---
+++ 4check_str_is_keyword.py
@@ -0,0 +1,14 @@
+import keyword
+
+print ("The list of keywords is : ")
+print (keyword.kwlist)
+
+keys = ["for", "while", "tanisha", "break", "sky",
+"elif", "assert", "pulkit", "lambda", "else", "sakshar"]
+ 
+for i in range(len(keys)):
+     # checking which are keywords
+    if keyword.iskeyword(keys[i]):
+        print(keys[i] + " is python keyword")
+    else:
+        print(keys[i] + " is not a python keyword")
