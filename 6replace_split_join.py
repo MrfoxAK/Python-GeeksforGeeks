ef1d4385790e5ab2fb4968ecf23302e1e1ca8e4f
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-08-15 00:53:33 +0530

    Add files via upload

---
+++ 6replace_split_join.py
@@ -0,0 +1,64 @@
+
+
+
+name = "Akash Das"
+
+old = "Akash"
+new = "Keya"
+
+namenew = name.replace(old,new)
+
+print(namenew)
+
+
+
+str = "Hello I am Ak"
+
+str1 = str.split(' ')
+
+print(str)
+print(str1)
+
+
+
+s = ["I","am","AK"]
+
+s1 = ' '.join(s)
+print(s1)
+
+
+# x,y = input("Enter two values : ").split()
+# x, y, z = input("Enter three values: ").split()
+
+# print(x)
+# print(y)
+# print(z)
+
+# x = list(map(int, input("Enter multiple values: ").split()))
+# print("List of students: ", x)
+# print(type(x[0]))
+
+
+# x,y = [int(x) for x in input("Enter two values: ").split()]
+# print(type(x))
+# print(y)
+
+
+# taking multiple inputs at a time separated by comma
+x = [int(x) for x in input("Enter multiple value: ").split(",")]
+print("Number of list is: ", x) 
+
+
+List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
+print("\nList with the use of Mixed Values: ")
+print(type(List[0]))
+
+
+
+
+
+
+
+
+
+
