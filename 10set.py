f0cda7fe6023f97d90f7b02bedd842a2fa865731
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-08-21 22:48:25 +0530

    Add files via upload

---
+++ 10set.py
@@ -0,0 +1,34 @@
+
+# s = set()
+# print(type(s))
+
+
+# l=[1,3,2,5]
+# s_l = set(l)
+
+
+# print(s_l)
+
+
+
+s = set()
+
+s.add(1)
+s.add(4)
+s1 = s.intersection({2,4})
+print(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
