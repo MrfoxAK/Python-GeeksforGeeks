1df08cb17795c9cde757c82a4a54737ae7119c38
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-08-21 22:48:39 +0530

    Add files via upload

---
+++ 11for_loop.py
@@ -0,0 +1,34 @@
+# l1 = ["ak","ek","tk"]
+
+
+# for i in l1:
+#      print(i)
+
+# l1 = [["ak",2],["ek",8],["tk",5]]
+
+
+# for i,k in l1:
+#      print(i,"and",k)
+
+l1 = [["ak",2],["ek",8],["tk",5]]
+
+dict1 = dict(l1)
+
+for i,k in dict1.items():
+     print(i,k)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
